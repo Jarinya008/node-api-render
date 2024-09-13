364a29c3cba0a9944dd887a7a72a1b1f06753259
Jarinya008 <jarinyawhan@gmail.com>
2024-09-14 01:40:03 +0700

    fdd

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
   res.send('home'); // ตรวจสอบว่า 'home.ejs' อยู่ในไดเรกทอรี 'views'
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
